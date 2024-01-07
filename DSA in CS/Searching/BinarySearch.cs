080702cd01996586cf7cfef4674dc8e9085b22fb
gauravj01 <jaiswalgauravshyam@gmail.com>
2024-01-07 19:37:22 +0530

    Implementation of Binary Search

--- DSA in CS/Searching/BinarySearch.cs
+++ DSA in CS/Searching/BinarySearch.cs
@@ -10,39 +10,34 @@ namespace DSA_in_CS.Searching
     {
         public int BinarySearchValue(int[] myArray, int value)
         {
-            if(myArray.Length == 0)
+            int start = 0;
+            int end = myArray.Length - 1;
+            int mid = (start + end) / 2;
+
+            if (myArray.Length == 0)
             {
                 return -1;
             }
             else
             {
-                return BinarySearchRecursion(myArray, value, 0, myArray.Length-1);
+                while (start <= end)
+                {
+                    mid = (start + end) / 2;
+                    if (myArray[mid] == value)
+                    {
+                        return mid;
+                    }
+                    else if (myArray[mid] > value)
+                    {
+                        end = mid-1;
+                    }
+                    else
+                    {
+                        start = mid+1;
+                    }
+                }
+                return -1;
             }
-        }
-
-        //10, 20, 30, 40, 50, 60 -> 0,5
-
-        //10, 20, 30, 40, 50 -> 0,4
-
-        public int BinarySearchRecursion(int[] myArray, int value, int start, int end)
-        {
-            int mid = (start + end)/2;
-            int pos = 0;
-            if (myArray[mid] > value)
-            {
-                start = mid;
-                BinarySearchRecursion(myArray, value, start, end);
-            }
-            else if(myArray[mid] < value)
-            {
-                end = mid;
-                BinarySearchRecursion(myArray, value, start, end);
-            }
-            else
-            {
-                pos = mid;
-            }
-            return pos;
         }
     }
 }
